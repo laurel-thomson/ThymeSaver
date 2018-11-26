5675a3a4eaf72180a640875b0432d1e8f862649c
laurel-thomson <laurel228@ksu.edu>
2018-11-25 20:19:25 -0600

    initial commit

---
+++ app/src/main/java/com/example/laure/thymesaver/WeeklyMealsActivity.java
@@ -0,0 +1,13 @@
+package com.example.laure.thymesaver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeeklyMealsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_meals);
+    }
+}
