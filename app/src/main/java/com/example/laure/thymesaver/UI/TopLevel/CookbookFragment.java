4fd4f766d2a680ebc2171c649df1555633fb1aab
laurel-thomson <laurel228@ksu.edu>
2018-12-14 13:21:40 -0600

    Adding recipes from database to cookbook fragment

--- app/src/main/java/com/example/laure/thymesaver/UI/TopLevel/CookbookFragment.java
+++ app/src/main/java/com/example/laure/thymesaver/UI/TopLevel/CookbookFragment.java
@@ -1,16 +1,54 @@
 package com.example.laure.thymesaver.UI.TopLevel;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.laure.thymesaver.Adapters.RecipeAdapter;
+import com.example.laure.thymesaver.Models.Recipe;
 import com.example.laure.thymesaver.R;
+import com.example.laure.thymesaver.ViewModels.RecipeViewModel;
+
+import java.util.List;
 
 public class CookbookFragment extends Fragment {
+    private RecipeViewModel mViewModel;
+    private RecipeAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_cookbook, viewGroup, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
+
+        mViewModel.getAllRecipes().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(@Nullable List<Recipe> recipes) {
+                //update the cached copy of recipes in the adapter
+                mAdapter.setRecipes(recipes);
+            }
+        });
+
+        mRecyclerView = view.findViewById(R.id.recipes_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new RecipeAdapter(getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
     }
 }
