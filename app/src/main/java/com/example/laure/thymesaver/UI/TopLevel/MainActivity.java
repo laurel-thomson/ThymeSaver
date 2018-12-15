84b4998a7fcf993ae828c318cce90b7d2e18e712
laurel-thomson <laurel228@ksu.edu>
2018-12-15 13:19:08 -0600

    Using ViewPager in MainActivity to allow swiping between Fragments

--- app/src/main/java/com/example/laure/thymesaver/UI/TopLevel/MainActivity.java
+++ app/src/main/java/com/example/laure/thymesaver/UI/TopLevel/MainActivity.java
@@ -4,13 +4,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.example.laure.thymesaver.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    
+    private MenuItem mPreviousMenuItem;
+    private BottomNavigationView mNavigationView;
+    private ViewPager mViewPager;
+    private ViewPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,38 +30,80 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        //Manually displaying the first fragment - one time only
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, new MealPlannerFragment());
-        transaction.commit();
-
-        BottomNavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setOnNavigationItemSelectedListener(
+        mNavigationView = findViewById(R.id.navigation);
+        mViewPager = findViewById(R.id.main_viewpager);
+        mNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        Fragment fragment = null;
                         switch(menuItem.getItemId())
                         {
                             case R.id.action_meal_planner:
-                                fragment = new MealPlannerFragment();
+                                mViewPager.setCurrentItem(0);
                                 break;
                             case R.id.action_cookbook:
-                                fragment = new CookbookFragment();
+                                mViewPager.setCurrentItem(1);
                                 break;
                             case R.id.action_pantry:
-                                fragment = new PantryFragment();
+                                mViewPager.setCurrentItem(2);
                                 break;
                             case R.id.action_shopping_list:
-                                fragment = new ShoppingListFragment();
+                                mViewPager.setCurrentItem(3);
                                 break;
                         }
-                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                        transaction.replace(R.id.frame_layout, fragment);
-                        transaction.commit();
                         return true;
                     }
                 }
         );
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+            @Override
+            public void onPageSelected(int position) {
+                if (mPreviousMenuItem != null) {
+                    mPreviousMenuItem.setChecked(false);
+                } else {
+                    mNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                mNavigationView.getMenu().getItem(position).setChecked(true);
+                mPreviousMenuItem = mNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mAdapter.addFragment(new MealPlannerFragment());
+        mAdapter.addFragment(new CookbookFragment());
+        mAdapter.addFragment(new PantryFragment());
+        mAdapter.addFragment(new ShoppingListFragment());
+        mViewPager.setAdapter(mAdapter);
+    }
+
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
     }
 }
