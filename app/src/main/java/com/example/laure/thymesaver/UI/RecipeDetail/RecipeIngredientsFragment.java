d43d6a055de5f4c49a4adac11b9d5a7029d7dc07
laurel-thomson <laurel228@ksu.edu>
2018-12-16 17:33:38 -0600

    Adding a RecyclerView to the RecipeIngredientsFragment

--- app/src/main/java/com/example/laure/thymesaver/UI/RecipeDetail/RecipeIngredientsFragment.java
+++ app/src/main/java/com/example/laure/thymesaver/UI/RecipeDetail/RecipeIngredientsFragment.java
@@ -1,19 +1,29 @@
 package com.example.laure.thymesaver.UI.RecipeDetail;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.laure.thymesaver.Adapters.IngredientAdapter;
 import com.example.laure.thymesaver.R;
 import com.example.laure.thymesaver.UI.AddIngredientsActivity;
+import com.example.laure.thymesaver.ViewModels.RecipeDetailViewModel;
 
 public class RecipeIngredientsFragment extends Fragment {
+    private RecipeDetailViewModel mViewModel;
+    private IngredientAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_recipe_ingredients, viewGroup, false);
@@ -22,6 +32,18 @@ public class RecipeIngredientsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mViewModel = ViewModelProviders.of(this).get(RecipeDetailViewModel.class);
+        mAdapter.setIngredients(mViewModel.getCurrentRecipeIngredients());
+
+        mRecyclerView = view.findViewById(R.id.recipe_ingredients_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new IngredientAdapter(getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
+
         FloatingActionButton button = view.findViewById(R.id.add_ingredient_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
