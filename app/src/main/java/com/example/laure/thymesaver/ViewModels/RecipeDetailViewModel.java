a887f2a27372bb06062a3e794ff79b857044aec8
laurel-thomson <laurel228@ksu.edu>
2018-12-16 17:47:49 -0600

    Adding a RecipeDetailViewModel to hold data on a single recipe for the RecipeDetailActivity

---
+++ app/src/main/java/com/example/laure/thymesaver/ViewModels/RecipeDetailViewModel.java
@@ -0,0 +1,34 @@
+package com.example.laure.thymesaver.ViewModels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+import com.example.laure.thymesaver.Firebase.Database.Repository;
+import com.example.laure.thymesaver.Models.Ingredient;
+import com.example.laure.thymesaver.Models.Recipe;
+
+import java.util.HashMap;
+
+public class RecipeDetailViewModel extends AndroidViewModel {
+    private Repository mRepository;
+    private String mCurrentRecipeName;
+    private Recipe mCurrentRecipe;
+
+    public RecipeDetailViewModel(Application application) {
+        super(application);
+        mRepository = Repository.getInstance();
+    }
+
+    public void setCurrentRecipe(String currentRecipeName) {
+        mCurrentRecipeName = currentRecipeName;
+        mCurrentRecipe = mRepository.getRecipe(currentRecipeName);
+    }
+
+    public Recipe getCurrentRecipe() {
+        return mCurrentRecipe;
+    }
+
+    public HashMap<Ingredient, Integer> getCurrentRecipeIngredients() {
+        return mRepository.getRecipeIngredients(mCurrentRecipe);
+    }
+}
