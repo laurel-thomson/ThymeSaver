e443076e8ea7cac7096935baa29f68d95631cf30
laurel-thomson <laurel228@ksu.edu>
2018-12-19 14:48:23 -0600

    Creating Add New Ingredient Activity

--- app/src/main/java/com/example/laure/thymesaver/UI/AddNewIngredientActivity.java
+++ app/src/main/java/com/example/laure/thymesaver/UI/AddNewIngredientActivity.java
@@ -1,7 +1,11 @@
 package com.example.laure.thymesaver.UI;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.laure.thymesaver.R;
 
@@ -11,5 +15,29 @@ public class AddNewIngredientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_ingredient);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Add Ingredient");
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_clear);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.action_save:
+                Toast.makeText(this, "Save clicked!", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_new_ingredient, menu);
+        return true;
     }
 }
