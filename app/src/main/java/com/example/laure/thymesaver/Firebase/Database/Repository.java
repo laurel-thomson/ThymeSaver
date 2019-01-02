74113d39d320362b650219495ae9da4d022b8e26
laurel-thomson <laurel228@ksu.edu>
2019-01-02 15:24:08 -0600

    Fixing shopping list deserializer to add ingredients from duplicate meal plans

--- app/src/main/java/com/example/laure/thymesaver/Firebase/Database/Repository.java
+++ app/src/main/java/com/example/laure/thymesaver/Firebase/Database/Repository.java
@@ -250,31 +250,27 @@ public class Repository {
         public HashMap<String, Integer> apply(DataSnapshot dataSnapshot) {
             mShoppingList.clear();
 
-            HashMap<String, HashMap<String, Integer>> mealPlans = new HashMap<>();
-            for (DataSnapshot snap : dataSnapshot.child("mealplan").getChildren()) {
-                MealPlan mealPlan = snap.getValue(MealPlan.class);
-                mealPlans.put(mealPlan.getRecipeName(), null);
-            }
-
-            for (DataSnapshot snap : dataSnapshot.child("recipes").getChildren()) {
-                if (mealPlans.containsKey(snap.getKey())) {
-                    Recipe r = snap.getValue(Recipe.class);
-                    mealPlans.put(snap.getKey(), r.getRecipeIngredients());
-                }
-            }
-
             HashMap<String, Integer> neededIngredients = new HashMap<>();
 
-            for (String recipeName : mealPlans.keySet()) {
-                for (String ingName : mealPlans.get(recipeName).keySet()) {
-                    if (!neededIngredients.containsKey(ingName)) {
-                        neededIngredients.put(ingName, mealPlans.get(recipeName).get(ingName));
-                    }
-                    else {
-                        neededIngredients.put(
-                                ingName,
-                                neededIngredients.get(ingName) + mealPlans.get(recipeName).get(ingName)
-                        );
+            for (DataSnapshot snap : dataSnapshot.child("mealplan").getChildren()) {
+                MealPlan mealPlan = snap.getValue(MealPlan.class);
+                String recipeName = mealPlan.getRecipeName();
+
+                for (DataSnapshot recipeSnap : dataSnapshot.child("recipes").getChildren()) {
+                    if (recipeSnap.getKey().equals(recipeName)) {
+                        Recipe recipe = recipeSnap.getValue(Recipe.class);
+                        for (String ingName : recipe.getRecipeIngredients().keySet()) {
+                            if (!neededIngredients.containsKey(ingName)) {
+                                neededIngredients.put(ingName, recipe.getRecipeIngredients().get(ingName));
+                            }
+                            else {
+                                neededIngredients.put(
+                                        ingName,
+                                        neededIngredients.get(ingName)
+                                                + recipe.getRecipeIngredients().get(ingName)
+                                );
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/laure/thymesaver/Firebase/Database/ShoppingListLiveData.java
+++ app/src/main/java/com/example/laure/thymesaver/Firebase/Database/ShoppingListLiveData.java
@@ -9,7 +9,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ShoppingListLiveData extends LiveData<DataSnapshot> {
     private HashMap<String, Integer> mShoppingList = new HashMap<>();
@@ -37,31 +40,27 @@ public class ShoppingListLiveData extends LiveData<DataSnapshot> {
             if(dataSnapshot != null){
                 setValue(dataSnapshot);
 
-                HashMap<String, HashMap<String, Integer>> mealPlans = new HashMap<>();
-                for (DataSnapshot snap : dataSnapshot.child("mealplan").getChildren()) {
-                    MealPlan mealPlan = snap.getValue(MealPlan.class);
-                    mealPlans.put(mealPlan.getRecipeName(), null);
-                }
-
-                for (DataSnapshot snap : dataSnapshot.child("recipes").getChildren()) {
-                    if (mealPlans.containsKey(snap.getKey())) {
-                        Recipe recipe = snap.getValue(Recipe.class);
-                        mealPlans.put(snap.getKey(), recipe.getRecipeIngredients());
-                    }
-                }
-
                 HashMap<String, Integer> neededIngredients = new HashMap<>();
 
-                for (String recipeName : mealPlans.keySet()) {
-                    for (String ingName : mealPlans.get(recipeName).keySet()) {
-                        if (!neededIngredients.containsKey(ingName)) {
-                            neededIngredients.put(ingName, mealPlans.get(recipeName).get(ingName));
-                        }
-                        else {
-                            neededIngredients.put(
-                                    ingName,
-                                    neededIngredients.get(ingName) + mealPlans.get(recipeName).get(ingName)
-                            );
+                for (DataSnapshot snap : dataSnapshot.child("mealplan").getChildren()) {
+                    MealPlan mealPlan = snap.getValue(MealPlan.class);
+                    String recipeName = mealPlan.getRecipeName();
+
+                    for (DataSnapshot recipeSnap : dataSnapshot.child("recipes").getChildren()) {
+                        if (recipeSnap.getKey().equals(recipeName)) {
+                            Recipe recipe = recipeSnap.getValue(Recipe.class);
+                            for (String ingName : recipe.getRecipeIngredients().keySet()) {
+                                if (!neededIngredients.containsKey(ingName)) {
+                                    neededIngredients.put(ingName, recipe.getRecipeIngredients().get(ingName));
+                                }
+                                else {
+                                    neededIngredients.put(
+                                            ingName,
+                                            neededIngredients.get(ingName)
+                                                    + recipe.getRecipeIngredients().get(ingName)
+                                    );
+                                }
+                            }
                         }
                     }
                 }
