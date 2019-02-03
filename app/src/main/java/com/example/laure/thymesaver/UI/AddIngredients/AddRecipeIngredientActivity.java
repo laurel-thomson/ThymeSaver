82f0aa7c7778a0e4168a059e7348d6541f0f3b73
laurel-thomson <laurel228@ksu.edu>
2019-02-03 17:14:47 -0600

    Redesigning AddRecipeIngredientActivity

--- app/src/main/java/com/example/laure/thymesaver/UI/AddIngredients/AddRecipeIngredientActivity.java
+++ app/src/main/java/com/example/laure/thymesaver/UI/AddIngredients/AddRecipeIngredientActivity.java
@@ -5,37 +5,79 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.example.laure.thymesaver.Models.Ingredient;
 import com.example.laure.thymesaver.Models.Recipe;
-import com.example.laure.thymesaver.Models.RecipeQuantity;
+import com.example.laure.thymesaver.R;
 import com.example.laure.thymesaver.ViewModels.PantryViewModel;
 import com.example.laure.thymesaver.ViewModels.RecipeDetailViewModel;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class AddRecipeIngredientActivity extends AppCompatActivity {
     public static String RECIPE_NAME = "My recipe name";
     private RecipeDetailViewModel mRecipeDetailViewModel;
     private PantryViewModel mPantryViewModel;
     private Recipe mRecipe;
+    private List<Ingredient> mTotalIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe_ingredients);
         mRecipeDetailViewModel = ViewModelProviders.of(this).get(RecipeDetailViewModel.class);
         String recipeName = getIntent().getStringExtra(RECIPE_NAME);
         mRecipeDetailViewModel.setCurrentRecipe(recipeName);
         mPantryViewModel = ViewModelProviders.of(this).get(PantryViewModel.class);
-        super.onCreate(savedInstanceState);
+        mPantryViewModel.getAllIngredients().observe(this, new Observer<List<Ingredient>>() {
+            @Override
+            public void onChanged(@Nullable List<Ingredient> ingredients) {
+                mTotalIngredients = ingredients;
+                List<String> names = new ArrayList<>();
+                for (Ingredient i : ingredients) {
+                    names.add(i.getName());
+                }
+                setUpIngredientNameTV(names);
+            }
+        });
         mRecipeDetailViewModel.getCurrentRecipe().observe(this, new Observer<Recipe>() {
             @Override
             public void onChanged(@Nullable Recipe recipe) {
                 mRecipe = recipe;
             }
         });
+        setUpUnitTV();
+    }
 
-        //todo update this activity
+    private void setUpUnitTV() {
+        final AutoCompleteTextView unitTV = findViewById(R.id.ingredient_unit);
+        ArrayAdapter<CharSequence> unitAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.ingredient_units,
+                android.R.layout.select_dialog_item);
+        unitTV.setThreshold(0);
+        unitTV.setAdapter(unitAdapter);
+        unitTV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                unitTV.showDropDown();
+                return true;
+            }
+        });
+    }
+
+    private void setUpIngredientNameTV(List<String> ingredientNames) {
+        final AutoCompleteTextView nameTV = findViewById(R.id.recipe_ingredient_name);
+        ArrayAdapter<String> nameAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.select_dialog_item,
+                ingredientNames);
+        nameTV.setThreshold(1);
+        nameTV.setAdapter(nameAdapter);
     }
 }
