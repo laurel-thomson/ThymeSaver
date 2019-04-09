3753f67118ae66e500247a8d4ef5fca61272abf7
laurel-thomson <laurel228@ksu.edu>
2019-04-09 00:39:01 -0500

    Adding recipe categories to the add planned meals activity

--- app/src/main/java/com/example/laure/thymesaver/Adapters/AddPlannedMealsAdapter.java
+++ app/src/main/java/com/example/laure/thymesaver/Adapters/AddPlannedMealsAdapter.java
@@ -3,6 +3,7 @@ package com.example.laure.thymesaver.Adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,18 +17,61 @@ import com.example.laure.thymesaver.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
-public class AddPlannedMealsAdapter extends RecyclerView.Adapter<AddPlannedMealsAdapter.MyViewHolder> {
-    private List<Recipe> mTotalRecipes;
+public class AddPlannedMealsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<Recipe> mTotalRecipes = new ArrayList<>();
     private List<Recipe> mPlannedRecipes = new ArrayList<>();
     private final LayoutInflater mInflater;
+    private Context mContext;
+    private static final int RECIPE_TYPE = 1;
+    private static final int HEADER_TYPE = 2;
 
     public AddPlannedMealsAdapter(Context context) {
+        mContext = context;
         mInflater = LayoutInflater.from(context);
     }
 
     public void setTotalRecipes(List<Recipe> recipes) {
-        mTotalRecipes = recipes;
+        mTotalRecipes.clear();
+
+        //generate category headers
+        CharSequence[] categories = mContext.getResources().getStringArray(R.array.recipe_categories);
+        Recipe[] headers = new Recipe[categories.length];
+        for (int i = 0; i < categories.length; i++) {
+            headers[i] = new Recipe("", categories[i].toString());
+        }
+
+        //add the headers in to the list
+        for (Recipe r : headers) {
+            mTotalRecipes.add(r);
+        }
+
+        //add all the ingredients in under their headers
+        for (Recipe recipe : recipes) {
+            int position = 0;
+            for (int i = 0; i < headers.length; i++) {
+                if (headers[i].getCategory().equals(recipe.getCategory())) {
+                    position = mTotalRecipes.indexOf(headers[i]);
+                    break;
+                }
+            }
+            mTotalRecipes.add(position+1, recipe);
+        }
+
+        //remove any headers that don't have ingredients under them
+        ListIterator<Recipe> iterator = mTotalRecipes.listIterator();
+        while (iterator.hasNext()) {
+            Recipe recipe = iterator.next();
+            if (getItemViewType(recipe) == HEADER_TYPE) {
+                int nextIndex = iterator.nextIndex();
+                if (nextIndex >= mTotalRecipes.size() || getItemViewType(nextIndex) == HEADER_TYPE) {
+                    iterator.remove();
+                }
+            }
+        }
+
+        notifyDataSetChanged();
     }
 
     public List<Recipe> getPlannedRecipes() {
@@ -36,21 +80,56 @@ public class AddPlannedMealsAdapter extends RecyclerView.Adapter<AddPlannedMeals
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = mInflater.inflate(R.layout.checklist_item, parent, false);
-        return new MyViewHolder(v);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        switch (viewType) {
+            case RECIPE_TYPE:
+                view = LayoutInflater.from(mContext)
+                        .inflate(R.layout.checklist_item, parent, false);
+                return new RecipeViewHolder(view);
+            default:
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.section_header, parent, false);
+                return new SectionHeaderViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
-        Recipe recipe = mTotalRecipes.get(position);
-        holder.mNameTV.setText(recipe.getName());
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                holder.mCheckBox.setChecked(!holder.mCheckBox.isChecked());
-            }
-        });
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder viewHolder, int position) {
+        int itemViewType = getItemViewType(position);
+        if (itemViewType == RECIPE_TYPE) {
+            final RecipeViewHolder holder = (RecipeViewHolder) viewHolder;
+            Recipe recipe = mTotalRecipes.get(position);
+            holder.mNameTV.setText(recipe.getName());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    holder.mCheckBox.setChecked(!holder.mCheckBox.isChecked());
+                }
+            });
+        }
+        else {
+            SectionHeaderViewHolder headerViewHolder = (SectionHeaderViewHolder) viewHolder;
+            final String category = mTotalRecipes.get(position).getCategory();
+            headerViewHolder.sectionTitle.setText(category);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (TextUtils.isEmpty(mTotalRecipes.get(position).getName())) {
+            return HEADER_TYPE;
+        } else {
+            return RECIPE_TYPE;
+        }
+    }
+
+    public int getItemViewType(Recipe recipe) {
+        if (TextUtils.isEmpty(recipe.getName())) {
+            return HEADER_TYPE;
+        } else {
+            return RECIPE_TYPE;
+        }
     }
 
     @Override
@@ -59,11 +138,11 @@ public class AddPlannedMealsAdapter extends RecyclerView.Adapter<AddPlannedMeals
         return mTotalRecipes.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    class RecipeViewHolder extends RecyclerView.ViewHolder {
         CheckBox mCheckBox;
         TextView mNameTV;
 
-        public MyViewHolder(@NonNull View view) {
+        public RecipeViewHolder(@NonNull View view) {
             super(view);
             mCheckBox = view.findViewById(R.id.checklist_checkbox);
             mNameTV = view.findViewById(R.id.checklist_textview);
@@ -83,6 +162,18 @@ public class AddPlannedMealsAdapter extends RecyclerView.Adapter<AddPlannedMeals
                     }
                 }
             });
+        }
+    }
+
+    class SectionHeaderViewHolder extends RecyclerView.ViewHolder {
+        TextView sectionTitle;
+        Button addButton;
+
+        SectionHeaderViewHolder(View itemView) {
+            super(itemView);
+            sectionTitle = itemView.findViewById(R.id.header_text);
+            addButton = itemView.findViewById(R.id.header_add_button);
+            addButton.setVisibility(View.GONE);
         }
     }
 }
