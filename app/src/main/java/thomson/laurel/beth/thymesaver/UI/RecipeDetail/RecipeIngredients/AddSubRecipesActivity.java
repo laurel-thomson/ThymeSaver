f02537e88167212b8c32adb1a1b498129106308a
laurel-thomson <laurel228@ksu.edu>
2019-07-09 08:20:59 -0500

    Fixing bug where AddSubRecipesActivity didnt have an action bar

--- app/src/main/java/thomson/laurel/beth/thymesaver/UI/RecipeDetail/RecipeIngredients/AddSubRecipesActivity.java
+++ app/src/main/java/thomson/laurel/beth/thymesaver/UI/RecipeDetail/RecipeIngredients/AddSubRecipesActivity.java
@@ -32,6 +32,7 @@ public class AddSubRecipesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_view_layout);
         final ProgressBar progressBar = findViewById(R.id.recycler_view_progress);
