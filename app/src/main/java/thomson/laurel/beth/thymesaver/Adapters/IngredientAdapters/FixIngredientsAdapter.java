5c67f53c1b71de29bc040a2a5e156ee1c481ba5c
laurel-thomson <laurel.beth.thomson@gmail.com>
2019-10-31 07:24:40 -0700

    Creating layout for fix ingredients adapter

--- app/src/main/java/thomson/laurel/beth/thymesaver/Adapters/IngredientAdapters/FixIngredientsAdapter.java
+++ app/src/main/java/thomson/laurel/beth/thymesaver/Adapters/IngredientAdapters/FixIngredientsAdapter.java
@@ -3,13 +3,15 @@ package thomson.laurel.beth.thymesaver.Adapters.IngredientAdapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -60,14 +62,22 @@ public class FixIngredientsAdapter extends RecyclerView.Adapter<FixIngredientsAd
     @NonNull
     @Override
     public FixIngredientsAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
-        LinearLayout v = (LinearLayout) LayoutInflater.from(mContext)
-                .inflate(R.layout.checklist_item, parent, false);
+        View v = (View) LayoutInflater.from(mContext)
+                .inflate(R.layout.fix_ingredient_item, parent, false);
         return new FixIngredientsAdapter.MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        myViewHolder.mIngredientName.setText(mRecipeIngredients.get(i).getName());
+        Ingredient ing = mRecipeIngredients.get(i);
+        RecipeQuantity quantity = mRecipeQuantities.get(ing.getName());
+        myViewHolder.mNameTV.setText(ing.getName());
+        myViewHolder.mQuantityET.setText(Double.toString(quantity.getRecipeQuantity()));
+        myViewHolder.mUnitET.setText(quantity.getUnit());
+        myViewHolder.mNameET.setText(ing.getName());
+        myViewHolder.mCategoryET.setText(ing.getCategory());
+        myViewHolder.mIsBulkSwitch.setChecked(ing.isBulk());
+
     }
 
 
@@ -79,11 +89,21 @@ public class FixIngredientsAdapter extends RecyclerView.Adapter<FixIngredientsAd
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
-        private TextView mIngredientName;
+        private TextView mNameTV;
+        private EditText mQuantityET;
+        private EditText mUnitET;
+        private EditText mNameET;
+        private EditText mCategoryET;
+        private Switch mIsBulkSwitch;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            mIngredientName = itemView.findViewById(R.id.checklist_textview);
+            mNameTV = itemView.findViewById(R.id.name_textview);
+            mQuantityET = itemView.findViewById(R.id.quantity_edittext);
+            mUnitET = itemView.findViewById(R.id.unit_edittext);
+            mNameET = itemView.findViewById(R.id.name_edittext);
+            mCategoryET = itemView.findViewById(R.id.category_edittext);
+            mIsBulkSwitch = itemView.findViewById(R.id.is_bulk_switch);
         }
     }
 
